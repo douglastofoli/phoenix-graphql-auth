62763b746b91baae15a08aede4c48200e34679fd
douglastofoli <tofoli.douglas@hotmail.com>
2023-11-19 14:20:37 -0300

    add tool versions

--- flake.nix
+++ flake.nix
@@ -6,22 +6,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (pkgs.lib) optional optionals;
-        pkgs = import nixpkgs { inherit system; };
-        lexical-lsp = pkgs.beam.packages.erlangR26.callPackage ./lexical-lsp.nix {};
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (pkgs.lib) optional optionals;
+      pkgs = import nixpkgs {inherit system;};
 
-        inputs = with pkgs;
-          [ elixir_1_15 elixir-ls glibcLocales ]
-          ++ optional stdenv.isLinux [ inotify-tools ]
-          ++ optional stdenv.isDarwin terminal-notifier
-          ++ optionals stdenv.isDarwin
-          (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
-      in with pkgs; {
+      inputs = with pkgs;
+        [elixir_1_15 glibcLocales]
+        ++ optional stdenv.isLinux [inotify-tools]
+        ++ optional stdenv.isDarwin terminal-notifier
+        ++ optionals stdenv.isDarwin
+        (with darwin.apple_sdk.frameworks; [CoreFoundation CoreServices]);
+    in
+      with pkgs; {
         devShells.default = mkShell {
-          name = "blog";
+          name = "phoenix-graphql-auth";
           packages = inputs;
         };
       });
